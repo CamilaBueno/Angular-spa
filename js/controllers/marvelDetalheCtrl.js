e0fdef8e4d8c2b5c9b06c5e38943db8ea7909884
Camila Bueno <cbf.camila@gmail.com>
2018-10-01 11:02:25 -0300

    Primeira versão da SPA

--- js/controllers/marvelDetalheCtrl.js
+++ js/controllers/marvelDetalheCtrl.js
@@ -1,7 +1,7 @@
 angular.module("marvelColector").controller("marvelDetalheCtrl", function ($scope, marvelService, $routeParams) {
     var searchComics = function () {
         marvelService.getComicDetail($routeParams.id).success(function (data) {
-            $scope.comic = data.data.results;
+            $scope.comic = data.data.results[0];
         });
     };
 
